c3c90e2f5c7c46d1996930f7b66c089971ce1f30
emersondantas <emerson.ruan@dce.ufpb.br>
2020-05-09 17:42:42 -0300

    Added new exception handler

--- src/main/java/br/ufpb/dcx/apps4society/educapi/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/br/ufpb/dcx/apps4society/educapi/resources/exceptions/ResourceExceptionHandler.java
@@ -1,10 +1,12 @@
 package br.ufpb.dcx.apps4society.educapi.resources.exceptions;
 
+import javax.persistence.EntityNotFoundException;
 import javax.servlet.http.HttpServletRequest;
 
 import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.orm.jpa.JpaObjectRetrievalFailureException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -40,6 +42,12 @@ public class ResourceExceptionHandler {
 
 	@ExceptionHandler(ConstraintViolationException.class)
 	public ResponseEntity<StandardError> constraintViolationException(ConstraintViolationException e, HttpServletRequest request){
+		StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage() + " " + e.getCause().getMessage(), System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+	}
+
+	@ExceptionHandler(JpaObjectRetrievalFailureException.class)
+	public ResponseEntity<StandardError> entityNotFoundException(JpaObjectRetrievalFailureException e, HttpServletRequest request){
 		StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage() + " " + e.getCause().getMessage(), System.currentTimeMillis());
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
 	}
