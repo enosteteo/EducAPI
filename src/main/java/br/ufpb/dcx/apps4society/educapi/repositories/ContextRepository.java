d7fd539686a61295397398d9edac957332619c9b
Raimundo Marcos Gonçalves Ludgério <raimundo.marcos@dcx.ufpb.br>
2020-07-27 20:35:29 -0300

    Update ContextRepository.java

--- src/main/java/br/ufpb/dcx/apps4society/educapi/repositories/ContextRepository.java
+++ src/main/java/br/ufpb/dcx/apps4society/educapi/repositories/ContextRepository.java
@@ -4,10 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import br.ufpb.dcx.apps4society.educapi.domain.User;
 import br.ufpb.dcx.apps4society.educapi.domain.Context;
-
+import java.util.Optional;
 import java.util.List;
 
 @Repository
 public interface ContextRepository extends JpaRepository<Context, Long> {
-	List<Context> findContextsByCreator(User creator);
+	 Optional<List<Context>> findContextsByCreator(User creator);
 }
