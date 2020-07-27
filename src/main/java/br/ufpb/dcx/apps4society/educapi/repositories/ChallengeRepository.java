dd131a6ea0b91d99015b4532b07aa577a4a557d6
Raimundo Marcos Gonçalves Ludgério <raimundo.marcos@dcx.ufpb.br>
2020-07-27 20:22:22 -0300

    Update ChallengeRepository.java

--- src/main/java/br/ufpb/dcx/apps4society/educapi/repositories/ChallengeRepository.java
+++ src/main/java/br/ufpb/dcx/apps4society/educapi/repositories/ChallengeRepository.java
@@ -1,12 +1,14 @@
 package br.ufpb.dcx.apps4society.educapi.repositories;
-
+import br.ufpb.dcx.apps4society.educapi.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import br.ufpb.dcx.apps4society.educapi.domain.Challenge;;
+import br.ufpb.dcx.apps4society.educapi.domain.Challenge;
+import java.util.List;
+
 
 
 @Repository
 public interface ChallengeRepository extends JpaRepository<Challenge, Long> {
-	
+	List<Challenge> findChallengesByCreator(User creator);
 }
