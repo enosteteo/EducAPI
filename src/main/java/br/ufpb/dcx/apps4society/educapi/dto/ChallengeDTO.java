a1ecbb57785520efd26f76414946a6a1e66f595e
Raimundo Marcos Gonçalves Ludgério <raimundo.marcos@dcx.ufpb.br>
2020-07-27 20:18:16 -0300

    Update ChallengeDTO.java

--- src/main/java/br/ufpb/dcx/apps4society/educapi/dto/ChallengeDTO.java
+++ src/main/java/br/ufpb/dcx/apps4society/educapi/dto/ChallengeDTO.java
@@ -16,7 +16,7 @@ public class ChallengeDTO implements Serializable{
 	
 	private Long id;
 	@NotEmpty(message="Required")
-	@Length(min=5, max=50, message="The size must be between 5 and 50 characters")
+	@Length(min=2, max=72, message="The size must be between 2 and 72 characters"
 	private String word;
 	
 	private String soundUrl;
