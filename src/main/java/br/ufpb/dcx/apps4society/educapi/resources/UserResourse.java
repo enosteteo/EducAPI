8b165148c673644ac6e2eb139bbe83c4c816d628
Raimundo Marcos Gonçalves Ludgério <raimundo.marcos@dcx.ufpb.br>
2020-07-27 20:48:07 -0300

    Update UserResourse.java

--- src/main/java/br/ufpb/dcx/apps4society/educapi/resources/UserResourse.java
+++ src/main/java/br/ufpb/dcx/apps4society/educapi/resources/UserResourse.java
@@ -66,8 +66,8 @@ public class UserResourse {
 	@RequestMapping(value="/page", method=RequestMethod.GET, produces="application/json")
 	public ResponseEntity<Page<UserDTO>> findPage(
 			@RequestParam(value="page", defaultValue="0") Integer page, 
-			@RequestParam(value="linesPerPage", defaultValue="24") Integer linesPerPage, 
-			@RequestParam(value="orderBy", defaultValue="nome") String orderBy, 
+			@RequestParam(value="linesPerPage", defaultValue="10") Integer linesPerPage, 
+			@RequestParam(value="orderBy", defaultValue="name") String orderBy, 
 			@RequestParam(value="direction", defaultValue="ASC") String direction){
 		Page<User> list = service.findPage(page, linesPerPage, orderBy, direction);
 		Page<UserDTO> listDto = list.map(obj -> new UserDTO(obj));
