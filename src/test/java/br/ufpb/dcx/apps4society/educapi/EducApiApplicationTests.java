7ee8dc6f191f337d22cf835d0597fa3457f29873
AmandaAzevedo <amanda.azevedo@dcx.ufpb.br>
2020-08-03 19:37:23 -0300

    Update the project version and correct errors in the test class

--- src/test/java/br/ufpb/dcx/apps4society/educapi/EducApiApplicationTests.java
+++ src/test/java/br/ufpb/dcx/apps4society/educapi/EducApiApplicationTests.java
@@ -1,10 +1,10 @@
-package br.ufpb.dcx.apps4society.educandoapi;
+package br.ufpb.dcx.apps4society.educapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EducandoApiApplicationTests {
+class EducApiApplicationTests {
 
 	@Test
 	void contextLoads() {
