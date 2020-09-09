eca0a28dd3da0009ca5c86670eb35e534de53cb3
NapoleaoHerculano <francivaldo.napoleao@gmail.com>
2020-09-09 18:38:18 -0300

    Corrected exception inheritance

--- src/main/java/br/ufpb/dcx/apps4society/educapi/services/exceptions/InvalidUserException.java
+++ src/main/java/br/ufpb/dcx/apps4society/educapi/services/exceptions/InvalidUserException.java
@@ -1,12 +1,8 @@
 package br.ufpb.dcx.apps4society.educapi.services.exceptions;
 
-public class InvalidUserException extends IllegalArgumentException{
+    public class InvalidUserException extends Exception {
 
-    public InvalidUserException() {
-        super();
+        public InvalidUserException() {
+            super();
+        }
     }
-
-    public InvalidUserException(String s) {
-        super(s);
-    }
-}
