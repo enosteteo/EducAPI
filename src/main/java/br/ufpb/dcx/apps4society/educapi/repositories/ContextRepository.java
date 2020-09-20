7329b518bd79638ce811cf4e9ca00bc86f4ef39b
NapoleaoHerculano <francivaldo.napoleao@gmail.com>
2020-09-20 14:40:22 -0300

    Implementation of methods for querying with parameters

--- src/main/java/br/ufpb/dcx/apps4society/educapi/repositories/ContextRepository.java
+++ src/main/java/br/ufpb/dcx/apps4society/educapi/repositories/ContextRepository.java
@@ -3,6 +3,8 @@ package br.ufpb.dcx.apps4society.educapi.repositories;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import br.ufpb.dcx.apps4society.educapi.domain.User;
 import br.ufpb.dcx.apps4society.educapi.domain.Context;
@@ -11,5 +13,10 @@ import java.util.List;
 @Repository
 public interface ContextRepository extends JpaRepository<Context, Long> {
 	List<Context> findContextsByCreator(User creator);
-	Page<Context> findByNameStartsWithIgnoreCase(String name, Pageable pageable);
+	Page<Context> findAllByCreatorEmailLikeAndNameStartsWithIgnoreCase(String email, String name, Pageable pageable);
+
+	@Query("SELECT c FROM Context c JOIN c.creator u " +
+			"WHERE lower(c.name) LIKE lower(CONCAT(:name, '%')) " +
+			"OR u.email = :email")
+	Page<Context> findAllByEmailAndNameStartsWithIgnoreCase(@Param("name") String name, @Param("email") String email, Pageable pageable);
 }
