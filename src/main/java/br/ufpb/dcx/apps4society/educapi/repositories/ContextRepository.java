b4553497b34a7a506ae9bcd69996cb0e823b96e8
NapoleaoHerculano <francivaldo.napoleao@gmail.com>
2020-09-21 13:14:51 -0300

    Update search methods of API

--- src/main/java/br/ufpb/dcx/apps4society/educapi/repositories/ContextRepository.java
+++ src/main/java/br/ufpb/dcx/apps4society/educapi/repositories/ContextRepository.java
@@ -3,8 +3,6 @@ package br.ufpb.dcx.apps4society.educapi.repositories;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import br.ufpb.dcx.apps4society.educapi.domain.User;
 import br.ufpb.dcx.apps4society.educapi.domain.Context;
@@ -14,9 +12,6 @@ import java.util.List;
 public interface ContextRepository extends JpaRepository<Context, Long> {
 	List<Context> findContextsByCreator(User creator);
 	Page<Context> findAllByCreatorEmailLikeAndNameStartsWithIgnoreCase(String email, String name, Pageable pageable);
-
-	@Query("SELECT c FROM Context c JOIN c.creator u " +
-			"WHERE lower(c.name) LIKE lower(CONCAT(:name, '%')) " +
-			"OR u.email = :email")
-	Page<Context> findAllByEmailAndNameStartsWithIgnoreCase(@Param("name") String name, @Param("email") String email, Pageable pageable);
+	Page<Context> findAllByCreatorEmailEqualsIgnoreCase(String email, Pageable pageable);
+	Page<Context> findAllByNameStartsWithIgnoreCase(String name, Pageable pageable);
 }
